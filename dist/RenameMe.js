0d15c35c0a0ae00869922e1c71d4381014827867
Takuto Yoshikawa <takuto_yoshikawa@dwango.co.jp>
2014-10-03 19:42:32 +0900

    Support alias emoji

--- dist/RenameMe.js
+++ dist/RenameMe.js
@@ -1,66 +1,87 @@
 var apiToken = "YOUR API TOKEN HERE";
 var refreshMilliSeconds = 10 * 60 * 1000;
 
-function main() {
-  var slackEmojiProvider = new SlackEmojiProvider(apiToken, refreshMilliSeconds);
-  var slackEmojiReplacer = new SlackEmojiReplacer(slackEmojiProvider);
+(function(){
 
-  // add event listener for message insertion.
-  document.addEventListener("DOMNodeInserted", function(e) {
-    var line = e.target;
-    var msg = line.getElementsByClassName('message').item(0);
-      if (/http:/.test(msg.innerHTML)) return;
-    var replaced = slackEmojiReplacer.replace(msg.innerHTML);
-    msg.innerHTML = replaced;
-  }, false);
-}
+  function main() {
+    var slackEmojiProvider = new SlackEmojiProvider(apiToken, refreshMilliSeconds);
+    var slackEmojiReplacer = new SlackEmojiReplacer(slackEmojiProvider);
 
-SlackEmojiProvider = function(apiToken, refreshMilliSeconds) {
-  var emojiApiEndPoint = "https://slack.com/api/emoji.list";
-  var requestUrl = emojiApiEndPoint + "?token=" + apiToken;
-
-  var cache = {};
-
-  this.get = function(key) {
-    return cache;
-  };
-
-  function refresh() {
-    var r = new XMLHttpRequest();
-    r.open("GET", requestUrl, true);
-    r.onload = function() {
-      if (r.status == 200) {
-        cache = JSON.parse(r.responseText).emoji;
-      }
-    };
-    r.send();
+    // add event listener for message insertion.
+    document.addEventListener("DOMNodeInserted", function(e) {
+      var line = e.target;
+      var msg = line.getElementsByClassName('message').item(0);
+        if (/http:/.test(msg.innerHTML)) return;
+      var replaced = slackEmojiReplacer.replace(msg.innerHTML);
+      msg.innerHTML = replaced;
+    }, false);
   }
 
-  // refresh cache per 10 minutes.
-  setInterval(refresh(), refreshMilliSeconds);
+  var SlackEmoji = function(keyAndValue){
 
-  // refresh emoji list at construction.
-  refresh();
-};
-
-SlackEmojiReplacer = function(slackEmojiProvider) {
-  var nameRegex = /:([\w\d+\-_]+):/g;
-
-  this.replace = function(message) {
-    return message.replace(nameRegex, function(name) {
-      var iconName = name.substr(1, name.length-2);
-      var iconUrl = slackEmojiProvider.get()[iconName];
-      if(iconUrl === undefined) {
-        return name;
+    this.resolve = function (key) { return resolve(key); };
+  
+    function resolve(key) {
+      var value = keyAndValue[key];
+    
+      if (value !== null) {
+        if (value.lastIndexOf('alias', 0) === 0) {
+          return resolve( value.substr(value.indexOf(':') + 1, value.length) );
+        } else {
+          return value;
+        }
       } else {
-        return generateIconImgTag(iconUrl);
+        return undefined;
       }
-    });
+    }
   };
 
-  function generateIconImgTag(iconUrl) {
-    return '<img class="slack-emoji" src=' + iconUrl + '>';
-  }
-};
+  var SlackEmojiProvider = function(apiToken, refreshMilliSeconds) {
+    var emojiApiEndPoint = "https://slack.com/api/emoji.list";
+    var requestUrl = emojiApiEndPoint + "?token=" + apiToken;
 
-main();
+    var slackEmoji;
+
+    this.get = function () { return slackEmoji; };
+
+    function refresh() {
+      var r = new XMLHttpRequest();
+      r.open("GET", requestUrl, true);
+      r.onload = function() {
+        if (r.status == 200) {
+          slackEmoji = new SlackEmoji(JSON.parse(r.responseText).emoji);
+        }
+      };
+      r.send();
+    }
+
+    // refresh slackEmoji per specified minutes.
+    setInterval(refresh(), refreshMilliSeconds);
+
+    // refresh emoji list at construction.
+    refresh();
+  };
+
+  var SlackEmojiReplacer = function(slackEmojiProvider) {
+    var nameRegex = /:([\w\d+\-_]+):/g;
+
+    this.replace = function(message) {
+      return message.replace(nameRegex, function(name) {
+        var iconName = name.substr(1, name.length-2);
+        var iconUrl = slackEmojiProvider.get().resolve(iconName);
+        if(iconUrl === undefined) {
+          return name;
+        } else {
+          return generateIconImgTag(iconUrl);
+        }
+      });
+    };
+
+    function generateIconImgTag(iconUrl) {
+      return '<img class="slack-emoji" src=' + iconUrl + '>';
+    }
+  };
+  
+  main();
+
+})();
